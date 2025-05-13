da9939f75a50015d312cffef0e6e1ad802fa569e
TomasPavlovicPavlo <mr_pavlo@hotmail.com>
2025-05-13 13:56:28 +0200

    fw reverse

---
+++ bin2hex.py
@@ -0,0 +1,33 @@
+def convert(file):
+    while True:
+        num = file.read(1)
+        if num == b'':
+            break
+        # print(hex(int.from_bytes(num)))
+        out_0x.write(hex(int.from_bytes(num)))
+        out_0x.write(' ')
+        # print(f'{int.from_bytes(num):x}')
+        out.write(f'{int.from_bytes(num):02X}')
+        out.write(' ')
+
+
+
+file = "KX-T30810B_FW.BIN"
+
+out_file = "out.txt"
+out_file_0x = "out_0x.txt"
+
+text = open(file, "rb")
+
+out = open(out_file, "w")
+out_0x = open(out_file_0x, "w")
+
+
+convert(text)
+
+
+out.close()
+out_0x.close()
+
+
+print("end")
